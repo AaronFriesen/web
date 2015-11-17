4b7b3a8024f72fa2e68595bc60075313a86bdc44
afriesen6 <afriesen6@gatech.edu>
2015-11-17 02:32:13 -0800

    Remove junk files and remove redundant header/footer html.

---
+++ footer.js
@@ -0,0 +1,11 @@
+document.write(
+"  <div class='container'>" +
+"    <hr>" +
+"" +
+"    <footer>" +
+"      <p>Created by Aaron Friesen using Twitter Bootstrap</p>" +
+"      <p>Last updated November 2015</p>" +
+"" +
+"    </footer>" +
+"  </div><!-- /container -->"
+);
---
+++ header.js
@@ -0,0 +1,34 @@
+document.write(
+"<div class='navbar navbar-inverse navbar-fixed-top'>" +
+"    <div class='container'>" +
+"      <div class='navbar-header'>" +
+"        <a class='navbar-brand' href='index.html'>Aaron Friesen</a>" +
+"      </div>" +
+"      <div class='navbar-collapse collapse'>" +
+"        <ul class='nav navbar-nav'>" +
+"          <li id='header-home'><a href='index.html'>Home</a></li>" +
+"          <li id='header-about'><a href='about.html'>About</a></li>" +
+"          <li id='header-resume'><a href='AaronFriesenResume.pdf'>Resume</a></li>" +
+"          <li id='header-dropdown' class='dropdown'>" +
+"            <a class='dropdown-toggle' data-toggle='dropdown' href='#'>Work<b class='caret'></b></a>" +
+"            <ul class='dropdown-menu'>" +
+"              <li id='header-neurorush'><a href='neurorush.html'>NeuroRush</a></li>" +
+"              <li id='header-ta'><a href='ta.html'>Undergraduate Teaching Assistant</a></li>" +
+"              <li id='header-space-trader'><a href='spacetrader.html'>Space Trader</a></li>" +
+"              <li id='header-esp'><a href='esp.html'>Entertainment Software Producers</a></li>" +
+"              <li id='header-yelp'><a href='yelp.html'>Yelp</a></li>" +
+"            </ul>" +
+"          </li>" +
+"          <li class='dropdown'>" +
+"            <a class='dropdown-toggle' data-toggle='dropdown' href='#'>Contact<b class='caret'></b></a>" +
+"            <ul class='dropdown-menu'>" +
+"              <li><a href='https://www.linkedin.com/pub/aaron-friesen/6a/152/550/'>LinkedIn</a></li>" +
+"              <li><a href='https://www.facebook.com/fr0stbyte'>Facebook</a></li>" +
+"              <li><a href='mailto:aaron@aaron-friesen.com'>Email</a></li>" +
+"            </ul>" +
+"          </li>" +
+"        </ul>" +
+"      </div><!--/.navbar-collapse -->" +
+"    </div>" +
+"</div>"
+);
