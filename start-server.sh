7c2d2207f002e3361c542b08eed1bb06137366ff
afriesen6 <afriesen6@gatech.edu>
2016-09-11 23:30:29 -0700

    Update start server script

--- start-server.sh
+++ start-server.sh
@@ -2,4 +2,4 @@
 
 sudo cp config/ssl.conf /etc/httpd/conf.d/ssl.conf
 sudo service httpd stop
-sudo httpd -f /home/ec2-user/git/web/config/httpd.conf -k start
+sudo httpd -f `echo $PWD`/config/httpd.conf -k start
